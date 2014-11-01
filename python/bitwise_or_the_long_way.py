b0f58bf108c5bd720bcc7a5c6ca07ec511e5bf6d
tlake <tanner.lake@gmail.com>
2014-11-01 16:35:56 -0700

    made bitwise_or[...] more readable

--- python/bitwise_or_the_long_way.py
+++ python/bitwise_or_the_long_way.py
@@ -1,10 +1,16 @@
 # bitwise 'or' - the long way:
 def bitwise_or(x, y):
+    # i don't want the '0b' part of the binary inputs
     x = x[2::]
     y = y[2::]
+
+    # some other defs
     new_string = ""
     new_y = ""
     new_x = ""
+
+    # make sure that both imputs are the same length,
+    # then store them in new_x and new_y
     if len(x) > len(y):
         new_y = ((len(x) - len(y)) * "0") + y
         new_x = x
@@ -14,17 +20,25 @@ def bitwise_or(x, y):
     else:
         new_x = x
         new_y = y
+
+    # i'll need these lists for comparing the elements
     newer_x = []
     newer_y = []
+
+    # actually populating those lists
     for i in new_y:
-        newer_y.append(i)
+        newer_y.append(int(i))
     for i in new_x:
-        newer_x.append(i)
+        newer_x.append(int(i))
+
+    # this is where testing happens
     for i in newer_y:
-        if newer_x[int(i) - 1] == "1" or newer_y[int(i) - 1] == "1":
+        if newer_x[i] == 1 or newer_y[i] == 2:
             new_string += "1"
         else:
             new_string += "0"
+
+    # format and return
     new_string = "0b" + new_string
     return new_string
 
